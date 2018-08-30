9f9a0d9f519ed17e2f0a50b3e62487f81771e6d1
Misu <misu@fill-in-later.co.nz>
2018-08-30 10:12:30 +0300

    Expired normal item failing tick

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -15,4 +15,13 @@ public class GildedRoseTest {
         assertEquals(4, app.items[0].sellIn);
     }
 
+    @Test
+    public void When_ExpiredNormalItemTicks_Expect_QualityDecreasesByTwo() {
+        Item[] items = new Item[] { new Item("normal", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+    }
+
 }
