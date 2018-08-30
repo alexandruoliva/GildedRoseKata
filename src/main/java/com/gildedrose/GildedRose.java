a51bbaab290930a650e9e90394e264eaf81fe5b5
Misu <misu@fill-in-later.co.nz>
2018-08-30 12:41:09 +0300

    extract methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,42 +10,53 @@ class GildedRose {
     public void updateQuality() {
 
         for (Item item : items) {
-
             if (item.name == "normal") {
-                if (item.sellIn == 0) {
-                    if (item.quality > 1)
-                        item.quality -= 2;
-                    else
-                        item.quality = 0;
-                } else {
-                    item.quality -= 1;
-                    item.sellIn -= 1;
-                }
+                handleNormal(item);
             } else if (item.name == "Aged Brie") {
-                if (item.quality < 50)
-                    item.quality += 1;
-                if (item.sellIn > 0)
-                    item.sellIn -= 1;
+                handleAgedBrie(item);
             } else if (item.name == "Sulfuras") {
-                if (item.sellIn > 0)
-                    item.sellIn -= 1;
-
+                handleSulfuras(item);
             } else {
-                if (item.sellIn > 10) {
-                    item.sellIn -= 1;
-                    item.quality += 1;
-                } else if (item.sellIn > 5) {
-                    item.sellIn -= 1;
-                    item.quality += 2;
-                } else if (item.sellIn > 1) {
-                    item.sellIn -= 1;
-                    item.quality += 3;
-                } else {
-                    item.sellIn -= 1;
-                    item.quality = 0;
-                }
+                handleBackstagePasses(item);
             }
         }
+    }
+
+    private void handleNormal(Item item) {
+        if (item.sellIn == 0) {
+            if (item.quality > 1)
+                item.quality -= 2;
+            else
+                item.quality = 0;
+        } else {
+            item.quality -= 1;
+            item.sellIn -= 1;
+        }
+    }
+
+    private void handleAgedBrie(Item item) {
+        if (item.quality < 50)
+            item.quality += 1;
+        if (item.sellIn > 0)
+            item.sellIn -= 1;
+    }
+
+    private void handleSulfuras(Item item) {
+        if (item.sellIn > 0)
+            item.sellIn -= 1;
+    }
+
+    private void handleBackstagePasses(Item item) {
+        if (item.sellIn > 10) {
+            item.quality += 1;
+        } else if (item.sellIn > 5) {
+            item.quality += 2;
+        } else if (item.sellIn > 1) {
+            item.quality += 3;
+        } else {
+            item.quality = 0;
+        }
+        item.sellIn -= 1;
     }
 
 }
