126b979499f73a7428922618e913fda26a444927
Misu <misu@fill-in-later.co.nz>
2018-08-30 11:11:47 +0300

    Stinky cheese working

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,13 +8,21 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        if (items[0].sellIn == 0 ){
-            if( items[0].quality>1)
-                items[0].quality -= 2;
-            else
-                items[0].quality=0;
-        }else {
-            items[0].quality -= 1;
+
+        if (items[0].name=="normal") {
+            if (items[0].sellIn == 0) {
+                if (items[0].quality > 1)
+                    items[0].quality -= 2;
+                else
+                    items[0].quality = 0;
+            } else {
+                items[0].quality -= 1;
+                items[0].sellIn -= 1;
+            }
+        }
+        else
+        {
+            items[0].quality += 1;
             items[0].sellIn -= 1;
         }
     }
