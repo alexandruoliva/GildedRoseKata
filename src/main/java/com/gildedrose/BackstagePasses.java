3c0bb0d96ee88a710d89cdc4045055fa8c2c4d0e
unknown <olivaalexandru@Yahoo.com>
2018-09-01 20:19:38 +0300

    Backstage Passes class added, tests not working

---
+++ src/main/java/com/gildedrose/BackstagePasses.java
@@ -0,0 +1,22 @@
+package com.gildedrose;
+
+public class BackstagePasses extends Item{
+
+	public BackstagePasses( int sellIn, int quality) {
+		super("Backstage Passes", sellIn, quality);
+	}
+	
+	 	public void handleBackstagePasses() {
+	        if (this.sellIn > 10) {
+	            this.quality += 1;
+	        } else if (this.sellIn > 5) {
+	            this.quality += 2;
+	        } else if (this.sellIn > 1) {
+	            this.quality += 3;
+	        } else {
+	            this.quality = 0;
+	        }
+	        this.sellIn -= 1;
+	    }
+	
+}
--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,26 +17,8 @@ class GildedRose {
             } else if (item.name == "Sulfuras") {
             	((Sulfuras)item).handleSulfuras();
             } else {
-                handleBackstagePasses(item);
+            	((BackstagePasses)item).handleBackstagePasses();
             }
         }
     }
-
-    
-
-    
-
-    private void handleBackstagePasses(Item item) {
-        if (item.sellIn > 10) {
-            item.quality += 1;
-        } else if (item.sellIn > 5) {
-            item.quality += 2;
-        } else if (item.sellIn > 1) {
-            item.quality += 3;
-        } else {
-            item.quality = 0;
-        }
-        item.sellIn -= 1;
-    }
-
 }
