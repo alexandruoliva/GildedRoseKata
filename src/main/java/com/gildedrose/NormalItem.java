e41de31dc50ac101d4dc8296cdcf1b7aab9f3b14
unknown <olivaalexandru@Yahoo.com>
2018-09-02 15:45:38 +0300

    abstract class brokes TextTestFeature

--- src/main/java/com/gildedrose/NormalItem.java
+++ src/main/java/com/gildedrose/NormalItem.java
@@ -1,19 +1,19 @@
 package com.gildedrose;
 
-public class NormalItem extends Item{
-    public NormalItem(int sellIn, int quality) {
-        super("normal", sellIn, quality);
-    }
+public class NormalItem extends Item {
+	public NormalItem(int sellIn, int quality) {
+		super("normal", sellIn, quality);
+	}
 
-    public void handle() {
-        if (this.sellIn == 0) {
-            if (this.quality > 1)
-                this.quality -= 2;
-            else
-                this.quality = 0;
-        } else {
-            this.quality -= 1;
-            this.sellIn -= 1;
-        }
-    }
+	public void handle() {
+		if (this.sellIn == 0) {
+			if (this.quality > 1)
+				this.quality -= 2;
+			else
+				this.quality = 0;
+		} else {
+			this.quality -= 1;
+			this.sellIn -= 1;
+		}
+	}
 }
