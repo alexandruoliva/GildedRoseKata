53f9ce1719d2b5afa4b82379031ba5021fde2395
Oliva Alexandru <olivaalexandru@yahoo.com>
2018-09-02 16:55:12 +0300

    Expired conjured item decreasing by 4 works and also gains zero ,too much implementation :(

--- src/main/java/com/gildedrose/Conjured.java
+++ src/main/java/com/gildedrose/Conjured.java
@@ -1,13 +1,20 @@
 package com.gildedrose;
 
-public class Conjured extends Item{
+public class Conjured extends Item {
 
-	public Conjured( int sellIn, int quality) {
+	public Conjured(int sellIn, int quality) {
 		super("Conjured", sellIn, quality);
 	}
 	public void handle() {
-		this.sellIn -=1;
-		this.quality -=1;
+		if(this.sellIn==0) {
+		 if(this.quality>4)
+			 this.quality -=4;
+		else
+			this.quality=0;
+		}else {
+			this.quality -= 1;
+			this.sellIn -= 1;
+		}
 	}
 
 }
